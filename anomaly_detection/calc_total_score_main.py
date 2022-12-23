1fd045349ecab1d70e6317b1c2907656ed36c99e
okestro-dhkim3 <dh.kim3@okestro.com>
2022-12-23 15:56:06 +0900

    anomlydetection commit

--- anomaly_detection/calc_total_score_main.py
+++ anomaly_detection/calc_total_score_main.py
@@ -1,4 +1,3 @@
-# -- coding: utf-8 --
 import os
 import sys
 import configparser
@@ -13,20 +12,44 @@ from utils.logs.log import standardLog
 standardLog = standardLog()
 
 def total_main():
-    # configparser
     config = configparser.ConfigParser()
     abs_path = '/croffle'
     config.read(abs_path + "/config.ini")
 
-    # es config
     es_host=config.get('ES','HOST')
     es_port=config.get('ES','PORT')
     es_id=config.get('ES','USER')
     es_pw=config.get('ES','PASSWORD')
 
-    # es read
     try:
         es = Elasticsearch(hosts=f"http://{es_id}:{es_pw}@{es_host}:{es_port}/", timeout=1000)
     except Exception as e:
         standardLog.sending_log('error', e).error('AnomalyDetection Connect ES error')
         exit(1)
+
+    try:
+        total_anomaly = TotalAnomaly(es)
+    except Exception as e:
+        standardLog.sending_log('error', e).error(f'AnomalyDetection total init error')
+        exit(1)
+
+    try:
+        total_anomaly.retrieve_metric_anomaly()
+    except Exception as e:
+        standardLog.sending_log('error', e).error(f'AnomalyDetection retrieve metric error')
+        exit(1)
+    try:
+        total_anomaly.retrieve_log_anomaly()
+    except Exception as e:
+        standardLog.sending_log('error', e).error(f'AnomalyDetection retrieve log error')
+        exit(1)
+    try:
+        total_anomaly.update_total_anomaly_score()
+    except Exception as e:
+        standardLog.sending_log('error', e).error(f'AnomalyDetection update total score error')
+        exit(1)
+
+    standardLog.sending_log('success').info('AnomalyDetection calc total score success')
+
+if __name__ == '__main__':
+    total_main()
